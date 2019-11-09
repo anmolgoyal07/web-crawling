c77a98ac6f00207768f8a32285f1b6f2e83612d9
anmolgoyal07 <anmolgoyal07@gmail.com>
2019-11-09 19:14:23 +0530

    [DEV] adding intial code

---
+++ src/test/java/me/anmolgoyal/crawling/WebCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.anmolgoyal.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
